45413b7047fec83cc6d1f2f1f832b6dc97bc8e00
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-19 14:38:31 -0500

    finished exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,14 +1,17 @@
 ﻿namespace MethodsExercise
 {
     public class Program
-    {
+    { //-------------------exercise 1
+
         static void Main(string[] args)
+
         {
+
             Console.WriteLine("Hello - What is your name?");
             var userName = Console.ReadLine();
 
             Console.WriteLine($"Hi, {userName}. What is your favorite color?");
-            var color = Console.ReadLine(); 
+            var color = Console.ReadLine();
 
             Console.WriteLine($" {color} is an amaizing color! What is your favorite animal?");
             var animal = Console.ReadLine();
@@ -22,7 +25,42 @@
             Console.WriteLine($"Favorite Animal: {animal}");
             Console.WriteLine($"Favorite Band: {band}");
 
+            //---------------Exercise 2
 
+            Console.WriteLine("value: " + Add(10, 8, 3));
+
+            Console.WriteLine("value: " + Subtract(10, 8));
+
+            Console.WriteLine("value: " + Multiply(8, 3));
+
+            Console.WriteLine("value: " + Divide(10, 3));
+
+
+        }  
+
+
+        public static int Add(int num1, int num2, int num3)
+        {
+
+            return num1 + num2 + num3;
+
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+
+        }
+        public static int Multiply(int num2, int num3)
+        { 
+          return num2 * num3;
+
+
+        }
+        public static int Divide(int num1, int num3) 
+        {
+            return num1 / num3;
+        
         }
     }
 }
