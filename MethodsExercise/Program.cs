cde3d245ab3d9f68133c0c2ad4b0048af7007342
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-19 11:58:40 -0500

    finished the second lines

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello - What is your name?");
-            var UserName = Console.ReadLine();
+            var userName = Console.ReadLine();
 
-            Console.WriteLine($"Hi, {UserName}. What is your favorite color?");
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
             var color = Console.ReadLine(); 
 
             Console.WriteLine($" {color} is an amaizing color! What is your favorite animal?");
@@ -15,6 +15,12 @@
 
             Console.WriteLine("Awesome ! Now, What is your favorite band?");
             var band = Console.ReadLine();
+
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
 
 
         }
