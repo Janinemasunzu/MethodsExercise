667905359b393783670765e5bd9b8b2535609325
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-19 11:05:28 -0500

    first Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello - What is your name?");
+            var UserName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {UserName}. What is your favorite color?");
+            var color = Console.ReadLine(); 
+
+            Console.WriteLine($" {color} is an amaizing color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Awesome ! Now, What is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
